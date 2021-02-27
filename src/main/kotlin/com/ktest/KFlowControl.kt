8f7cf8ba43d58097390f28dcbadfb8c83d9e36b5
MB-study <guomiao526@gmail.com>
2021-02-27 16:26:44 +0800

    kotlin

---
+++ src/main/kotlin/com/ktest/KFlowControl.kt
@@ -0,0 +1,82 @@
+package com.ktest
+
+import kotlin.random.Random as r
+
+fun getLevel(score: Int): String = when(score){
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        in 60..69 -> "D"
+        else -> "E"
+}
+
+//if , when , for , while , do while
+fun main(args: Array<String>) {
+    var score = r.nextInt(101)
+    val pass = if(score >= 60) "Pass" else "Fail"
+    println("$score $pass")
+//when
+//100 - 90 "A" , 89 - 80 "B" , 79-70 "C" , 69-60 "D" , 59 - 0 "E"
+    when(score) {
+        in 90..100 -> println("A")
+        in 80..89 -> println("B")
+        in 70..79 -> println("C")
+        in 60..69 -> println("D")
+        else -> println("E")
+    }
+    var level = when(score){
+
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        in 60..69 -> "D"
+        else -> "E"
+    }
+    println(level)
+    level = when(score/10){
+        10, 9 ->"A"
+        8 -> "B"
+        7 -> "C"
+        6 -> "D"
+        else ->"E"
+    }
+    println(level)
+    println(getLevel(score))
+
+    //for
+    for(i in 1..10) {
+        print("$i ")
+    }
+    println()
+    for(i in 1..10 step 2) { // step 間隔
+        print("$i ")
+    }
+    println()
+    for(i in 10 downTo 1){
+        print("$i ")
+    }
+    println()
+    for(i in 1 until 10){ // 不包含10
+        print("$i ")
+    }
+    println()
+
+    //動動腦
+    //有一個 1-10亂數 ans ，給使用者來猜，猜對 bingo 有五次機會
+    //加入請猜小 / 大一點的提示
+    val ans = r.nextInt(10)+1
+    val max = 5
+    for(i in 1..max){
+        print("第 $i/$max 次: 請輸入數字: ")
+        //val guess = Scanner(System.`in`).nextInt()
+        val guess = readLine()!!.toInt()
+        if(guess == ans){
+            println("bingo")
+            break
+        }else if (guess > ans){
+            println("請猜小一點")
+        }else{
+            println("請猜大一點")
+        }
+    }
+}
---
+++ src/main/kotlin/com/ktest/KNull.kt
@@ -0,0 +1,14 @@
+package com.ktest
+
+fun main(args: Array<String>) {
+    val chinese: Int? = null
+    val english: Int? = 90
+    val math: Int? = 80
+    //若沒考放入 null, 計算總分的時候已0分計算之
+    //請計算總分
+    var sum = (chinese ?:0) + (english ?: 0) + (math ?:0)
+    println(sum)
+    var sum2 = chinese?.plus(english?:0) ?:0 // 我今天有國文才要加英文
+    println(sum2)
+
+}
