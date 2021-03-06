f4a6f1a6d5640980847dd64988356a9f750e2775
MB-study <guomiao526@gmail.com>
2021-03-06 16:39:38 +0800

    kotlin

--- src/main/kotlin/com/lab/Parking.kt
+++ src/main/kotlin/com/lab/Parking.kt
@@ -1,24 +1,28 @@
 package com.lab
 
+import kotlin.math.log2
 import kotlin.math.pow
 
 var num = 8 // 車位數量
-var p =18 // 停車狀態
+var p = 18 // 停車狀態
 
-fun Int.toBinary(len: Int):String {
-    return String.format("%" + len + "s", this.toString(2)).replace(" ","0")
+fun Int.toBinary(len: Int): String {
+    return String.format("%" + len + "s", this.toString(2)).replace(" ", "0")
 }
+
 fun main() {
-    println("目前停車狀態: ${p.toBinary(num)}")
-    print("請選擇車位(0-7): ")
-    var n = readLine()!!.toInt()
-    n = 2.toDouble().pow(n).toInt()
-    val isPass: Boolean = (p.and(n) == 0)
-    if(isPass){
-        p += n
-        println("最新車位狀態: ${p.toBinary(num)},p=${p}")
-    }else{
-        n = Math.pow(n.toDouble(),0.5).toInt()
-        println("${n}號車位無法停車")
+    while (true) {
+        println("目前停車狀態: ${p.toBinary(num)}")
+        print("請選擇車位(0-7): ")
+        var n = readLine()!!.toInt()
+        n = 2.toDouble().pow(n).toInt()
+        val isPass: Boolean = (p.and(n) == 0)
+        if (isPass) {
+            p += n
+            println("最新車位狀態: ${p.toBinary(num)},p=${p}")
+        } else {
+            n = log2(n.toDouble()).toInt()
+            println("${n}號車位無法停車")
+        }
     }
 }
